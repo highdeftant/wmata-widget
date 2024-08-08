28da8e164e2a6cd914f18745fbb15518c7820c57
highdeftant <a.allen.com@gmail.com>
2024-08-08 14:41:27 -0400

    some slight updates

--- ui/backend.py
+++ ui/backend.py
@@ -1,37 +1,33 @@
 #!/bin/env python3
 
-import requests
-import sys
-import configparser 
+import requests, sys, configparser
 from PySide6 import QtCore, QtWidgets, QtGui, QtHelp
 
-# ---- API URL's, Links to files -----
+# ----  API URL's, Links to files    -----
 
 alltrn_url = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/All"
 empt_url = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{}"
+
+
+# ----  Backend Functions/Elements  ----
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 wmatapi = config.get('API', 'key')
 headers = {'api_key': wmatapi,}
-
 getTrns = requests.get(alltrn_url, headers)
 trnInfo = getTrns.json()
 
-# ---- Backend Functions/Elements ----
-
-
 # Pulls the next 1-4 trains departing from the station
 def nextThree(trainfile):
-    #todo:
-    #this function should return one train, or 4 - completed 7/8
     if 'Trains' in trainfile:
         result = 'Line Dest Min\n'
         for train in trainfile['Trains'][:4]:
             if 'Line' in train and 'Destination' in train and 'Min' in train:
-                result += '{} {} {}\n'.format(train['Line'], train['Destination'], train['Min'])
-        return result
-
+                result += '{} {} {}\n'.format(train['Line'],
+                                            train['Destination'],
+                                            train['Min'])
+    return result
 
 # This function grabs the station code from the station name used in the parameter
 # todo: use either regex or standard string methods to match incorrect names for ease of use
@@ -39,21 +35,25 @@ def getStatCode(stationName):
     stationcode = []
     statname = trnInfo['Trains'] #list of dictionaries - trains 
     for trains in statname:
-        if trains.get('LocationName') == stationName:
+        if trains.get('LocationName').lower() == stationName:
             stationcode.append(trains.get('LocationCode'))
             break
     return stationcode[0]
 
+
 # Grabs every station name and writes it to a file for use on the frontend search engine
 # todo: currently grabs multiple names of the same station when you only need one
-def trainNames(): 
+def trainNames():
     statname = trnInfo['Trains']
     stations = [train['LocationName'] for train in statname]
     with open('train_name_list.txt', 'w') as statnames:
-        for i in stations:
-            statnames.write(i)
-            statnames.write('\n')
-    return 
+        stationlist = []
+        for name in stations:
+            if name not in stationlist:
+                statnames.write(name)
+                statnames.write('\n')
+                stationlist.append(name)
+    return
 
 # Can't really explain what this does cause I can't remember lol
 def printTime(stationcode=''):
@@ -62,11 +62,12 @@ def printTime(stationcode=''):
     pulltime = statPull.json()
     return nextThree(pulltime)
 
-
 # Want this to show station info per station code in the parameter
 def showTrns():
-    pass 
+    pass
 
 
 #print(trnInfo)
-
+#lineup()
+if __name__ == "__main__":
+    trainNames()
--- ui/frontend.py
+++ ui/frontend.py
@@ -1,8 +1,7 @@
 #!/bin/env python3
-
 import backend
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui, QtHelp
+from PySide6 import QtCore, QtQuick, QtWidgets, QtGui, QtHelp
 
 # ----Frontend Elements----
 
@@ -11,33 +10,39 @@ class Frontend(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        # Window
+    # ----  Window  ----
+# These elements build the GUI Window
+
         self.setWindowTitle("WMATA Trains")
         self.button = QtWidgets.QPushButton("Update Times")
         self.text = QtWidgets.QLabel("Text to be updated",
                                     alignment=QtCore.Qt.AlignCenter)
 
-
-    # ----Search Engine----        
+    # ----  Search Engine   ----        
+# Functions/Attributes for search engine
 
         self.enginecore = QtHelp.QHelpEngineCore('../train_name_list.txt')
         self.searchengine = QtHelp.QHelpSearchEngine(self.enginecore)
-        self.searchbar = QtHelp.QHelpSearchQueryWidget(self) 
+        self.searchbar = QtHelp.QHelpSearchQueryWidget(self)
         self.results = QtHelp.QHelpSearchResultWidget(self)
         self.layout = QtWidgets.QVBoxLayout(self)
         self.data = self.enginecore.setupData()
 
+    # ----  Layout   ----
+# Sets the layout for the windows
+
         self.layout.addWidget(self.searchbar)
         self.layout.addWidget(self.results)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+    #    self.layout.addWidget(self.button)
 
-    # ----Button Functions----
+    # ----  Button Functions    ----
 
         self.button.clicked.connect(self.trainTime)
         self.searchbar.search.connect(self.search)
         self.searchbar.search.connect(self.trainTime)
 
+    #Uses textfile from backend to search for station names
     @QtCore.Slot()
     def search(self):
         station = self.searchbar.searchInput()
@@ -50,11 +55,17 @@ class Frontend(QtWidgets.QWidget):
     def trainTime(self):
         code = backend.getStatCode(self.searchengine.searchInput())
         self.text.setText(backend.printTime(code))
-     
+
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
+
+    with open("ui/style.qss", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
+
     widget = Frontend()
-    widget.resize(300, 180)
+    widget.resize(340, 230)
     widget.show()
     sys.exit(app.exec())
-
